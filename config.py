0a49730d1a7d37bef8efed028fff2e3f061f882d
Gabriel <gabrielfmm@al.insper.edu.br>
2022-11-26 18:08:16 -0300

    Config atualizada

---
+++ config.py
@@ -0,0 +1,9 @@
+# Dados gerais do jogo.
+WIDTH = 288 # Largura da tela
+HEIGHT = 512 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define algumas variáveis com as cores básicas
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
