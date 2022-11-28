31f9b8d038ba2308e733df5571de4d1db9fafda3
Gabriel <gabrielfmm@al.insper.edu.br>
2022-11-28 01:49:59 -0300

    Atualizada a config do jogo

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ BLACK = (0, 0, 0)
 
 # Variáveis
 altura_tela = 0.8 * HEIGHT
+velocidade = 0
+inicio_jogo = False
+fim_jogo = False
+reiniciar = False
+pontuacao = 0
+tela_de_inicio = True
+acerto_cano = False
+frequencia_cano = 1600 # frequência em milissegundos do aparecimento dos canos
