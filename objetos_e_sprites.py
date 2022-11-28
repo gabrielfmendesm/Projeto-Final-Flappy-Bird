3d21a8fab427cfdb6fb46a773e33f3bc2068f226
LuigiZema <105470377+LuigiZema@users.noreply.github.com>
2022-11-28 18:44:50 -0300

    Adicionado funções de animação do personagem

--- objetos_e_sprites.py
+++ objetos_e_sprites.py
@@ -11,6 +11,7 @@ class Bird: # personagem
         for i in range(1,4):
             img = image.load(f'Assets/Bird/bird{i}.png')
             self.lista.append(img)
+
         self.reset()
 
 	def update(self): # gravidade e movimento do bird
@@ -42,3 +43,36 @@ class Bird: # personagem
 			self.image = transform.rotate(self.lista[self.frame], self.angle)
 		
 		self.window.blit(self.image, self.rect)
+
+	def asa_contador(self): 
+		self.contador += 1
+		if self.contador > 5:
+			self.contador = 0
+			self.frame += 1
+		if self.frame >= 3:
+			self.frame = 0
+			
+	def asa_desenho(self):
+		self.asa_contador()
+		if self.posicao_asa <= -10 or self.fposicao_asa > 10:
+			self.inclinacao_asa *= -1
+		self.posicao_asa += self.inclinacao_asa
+		self.rect.y += self.inclinacao_asa
+		self.rect.x = WIDTH // 2 - 20
+		self.image = self.lista[self.frame]
+		self.window.blit(self.image, self.rect)
+		
+	def reset(self):
+		self.frame = 0
+		self.image = self.lista[self.iframe]
+		self.rect = self.image.get_rect()
+		self.rect.x = 60
+		self.rect.y = int(altura_tela) // 2
+		self.contador = 0
+		self.velocidade = 0
+		self.pulo = False
+		self.vivo = True
+		self.theta = 0
+		self.mid_pos = altura_tela // 2
+		self.posicao_asa = 0
+		self.inclinacao_asa= 1
