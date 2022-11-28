d5f18649a4c56e08bf14d29ac2eebde40490b737
LuigiZema <105470377+LuigiZema@users.noreply.github.com>
2022-11-28 18:18:58 -0300

    adionado gravidade e movimento ao personagem

--- objetos_e_sprites.py
+++ objetos_e_sprites.py
@@ -1,5 +1,7 @@
 from pygame import *
 from random import *
+from config import *
+from assets import *
 
 class Bird: # personagem 
 
@@ -11,3 +13,33 @@ class Bird: # personagem
             img = image.load(f'Assets/Bird/bird{i}.png')
             self.lista.append(img)
         self.reset()
+
+	def update(self): # gravidade e movimento do bird
+        
+        # gravidade ------------------------------------
+		self.velocidade += 0.3
+		if self.velvocidade >= 8:
+			self.velocidade = 8
+		if self.rect.bottom <= altura_tela:
+			self.rect.y += int(self.velocidade)
+
+		if self.vivo:
+		# pulo ------------------------------------------
+			if mouse.get_pressed()[0] == 1 and not self.pulo:
+				som_asa.play()
+				self.pulo = True
+				self.velocidade = -6
+			if mouse.get_pressed()[0] == 0:
+				self.pulo = False
+			
+			self.contador_asa()
+
+			self.image = transform.rotate(self.lista[self.frame], self.velocidade * -2)
+
+		else:
+
+			if self.rect.bottom <= altura_tela:
+				self.angle -= 2
+			self.image = transform.rotate(self.lista[self.frame], self.angle)
+		
+		self.window.blit(self.image, self.rect)
