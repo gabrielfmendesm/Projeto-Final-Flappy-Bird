601f8a6271fb638dabd1faa3f70c7bd7d28eca02
Gabriel <gabrielfmm@al.insper.edu.br>
2022-11-29 22:11:28 -0300

    Atualizado o código do jogo

--- jogo.py
+++ jogo.py
@@ -44,6 +44,34 @@ while rodando:
 				grupo_canos.add(top)
 				grupo_canos.add(bottom)
 				ultimo_cano = proximo_cano
+		
+		grupo_canos.update(velocidade)
+		base.update(velocidade)	
+		bird.update()
+		imagem_placar.update(pontuacao)
+		
+		if sprite.spritecollide(bird, grupo_canos, False) or bird.rect.top <= 0:
+			inicio_jogo = False
+			if bird.vivo:
+				som_impacto.play()
+				som_gameover.play()
+			bird.vivo = False
+			bird.theta = bird.vel * -2
+	
+		if bird.rect.bottom >= altura_tela:
+			velocidade = 0
+			fim_jogo = True
+	
+		if len(grupo_canos) > 0:
+			c = grupo_canos.sprites()[0]
+			if bird.rect.left > c.rect.left and bird.rect.right < c.rect.right and not cano_passado and bird.vivo:
+				cano_passado = True
+	
+			if cano_passado:
+				if bird.rect.left > c.rect.right:
+					cano_passado = False
+					pontuacao += 1
+					som_acerto.play()
 
 
 
